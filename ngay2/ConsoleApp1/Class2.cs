928b13400b7eb5f3e35d783d32c0ada25d4c58cf
LAPTOP-KUH98PUK\asus <mvhoang085@gmail.com>
2023-08-22 22:14:39 +0700

    B8,10,11,12 xuanthulab

---
+++ ngay2/ConsoleApp1/Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay2
+{
+    internal class Vukhi
+    {
+        public string name = "ten vu khi";
+        public int dosatthuong = 0;
+
+
+        //Thuoc tinh
+        public string Noisanxuat { set; get; }
+        public int Satthuong
+        {
+            //=
+            set
+            {
+                dosatthuong = value;
+            }
+            //truycap
+            get
+            {
+                return dosatthuong;
+            }
+        }
+        public Vukhi()
+        {
+            Console.WriteLine("Khoi tao");
+            dosatthuong = 1;
+        }
+        public Vukhi(string name, int _dosatthuong)
+        {
+            dosatthuong = _dosatthuong;
+            this.name = name;
+        } 
+        public void Thietlaptnacong(int dosatthuong)
+        {
+            this.dosatthuong = dosatthuong;
+        }
+        public void tancong()
+        {
+            Console.Write($"{name}: \t");
+            for (int i = 0; i < dosatthuong; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ngay2/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay2
+{
+    class Program
+    {
+
+        /*public static void binhphuong(int a, out int kq)
+        {
+            kq = a * a;
+            Console.WriteLine(a);
+        }
+        public static void Main(string[] args)
+        {
+            xinchao(ho:"Mai VIet",ten: "Hoang");
+            int a = 34, b = 45;
+            float x = 3.4f, y = 4.6f;
+            Console.WriteLine(TInhtoan.Tinhtoan.tong(a, b));
+            Console.WriteLine(TInhtoan.Tinhtoan.tong(x, y));
+            int c;
+            binhphuong(4, out c);
+            Console.WriteLine(c);
+
+        }
+
+        static void xinchao(string ten, string ho)
+        {
+            string fullname;
+            fullname = ho + " " + ten;
+            Console.WriteLine($"XIn chao {fullname}");
+        }*/
+
+        static void Main(string[] args)
+        {
+            Vukhi sungluc; //null
+            sungluc = new Vukhi();
+            sungluc.name = "sung luc: ";
+            sungluc.Thietlaptnacong(5);
+            sungluc.Noisanxuat = "My";
+
+            Console.WriteLine(sungluc.Noisanxuat);
+
+
+            Vukhi sungmay = new Vukhi("sung may",10);
+            sungluc.tancong();
+            sungmay.tancong();
+
+        }
+
+
+
+
+
+    }
+}
