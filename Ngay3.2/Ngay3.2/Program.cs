eb9a7548e3b7c1a795652c779f6cf4ff27ce8445
LAPTOP-KUH98PUK\asus <mvhoang085@gmail.com>
2023-08-25 22:39:13 +0700

    B13,14,15 xuanthulab

---
+++ Ngay3.2/Ngay3.2/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay3._2
+{
+    class Animal
+    {
+        public int legs { set; get; }
+        public float weight { set; get; }
+        public Animal()
+        {
+            Console.WriteLine("Khoi tao animal");
+        }
+        public Animal(string abc)
+        {
+            Console.WriteLine($"khoi tao animal (2)-{abc}");
+        }
+        public void Showlegs()
+        {
+            Console.WriteLine($"legs: {legs}");
+
+        }
+
+    }
+    class Cat : Animal
+    {
+        public string food;
+
+        public Cat(string s) : base(s)
+        {
+            this.legs = 4;
+            this.food = "fish";
+            Console.WriteLine("Khoi tao Cat");
+        }
+        public void Eat()
+        {
+            Console.WriteLine(food);
+        }
+        public new void Showlegs()
+        {
+            Console.WriteLine($"Loai meo co so chan la: {legs}");
+
+        }
+        public void ShowInfor()
+        {
+            base.Showlegs();
+            Showlegs();
+        }
+    }
+    class A { }
+    class B:A { }
+    class C:B { }
+
+    class Program
+    {
+        /*public struct Product
+        {
+            public string name;
+            public double gia;
+
+            public string infor
+            {
+                get{
+                    return $"{name}, {gia}";
+                }
+            }
+            public string Getinfo()
+            {
+                return $"Ten san pham {name}, gia: {gia}$";
+            }
+            public Product(string _name, double _price)
+            {
+                name = _name;
+                this.gia = _price;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Product sanpham1;
+            sanpham1.name = "ip5";
+            sanpham1.gia = 1000;
+
+            Product sanpham2 = new Product("ip7", 10000);
+
+            Console.WriteLine(sanpham1.Getinfo());
+            Console.WriteLine(sanpham2.Getinfo());
+            Console.WriteLine(sanpham2.infor);
+
+        }*/
+
+
+
+        //enum
+        /*enum Hocluc { 
+            kem =2, //0
+            trungbinh=5, //1 
+            kha=7, //2
+            gioi=8 //3
+        }
+        static void Main(string[] args)
+        {
+            Hocluc hocluc;
+            //hocluc = Hocluc.gioi;
+            hocluc = (Hocluc)(7);
+
+            switch (hocluc)
+            {
+                case Hocluc.kem:
+                    Console.WriteLine("kem");
+                    break;
+                case Hocluc.trungbinh:
+                    Console.WriteLine("tb");
+                    break;
+                case Hocluc.kha:
+                    Console.WriteLine("kha");
+                    break;
+                case Hocluc.gioi:
+                    Console.WriteLine("gioi");
+                    break;
+            }
+        }
+        */
+
+
+        //Tinhs kees thuwaf
+        static void Main(string[] args)
+        {
+            Cat c = new Cat("ABC sux");
+            c.ShowInfor();
+            c.Eat();  
+        }
+
+
+    }
+}
---
+++ Ngay3/Ngay3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string loichao="xin chao";
+            string ten = "MVH";
+
+            string thongbao = loichao + ten;
+            thongbao += "!";
+            Console.WriteLine(thongbao);*/
+            //string thongbao;
+            //thongbao = "\\Hoc\t ve\t chuoi ky tu \"String\"";
+            /*thongbao = @"Xin chao     2023
+
+                23141";*/
+            //string name = "MVH";
+            //int tuoi = 20;
+            //string gioitinh = "nam";
+            /*thongbao = $@"
+Ho ten: {name,-10}
+Tuoi: {tuoi,-10}
+Gioi tinh: {gioitinh,-10}
+
+";*/
+            /*thongbao = "lady and gentlyment";
+            int dodai = thongbao.Length;
+            char c = thongbao[3];
+
+
+            Console.WriteLine(dodai);
+            Console.WriteLine(thongbao);
+            Console.WriteLine(c);
+            for(int i = 0; i < dodai; i++)
+            {
+                char b = thongbao[i];
+                Console.WriteLine($"Chi so {i} la ky tu: {b,3}");
+            }
+            foreach(var kytu in thongbao)
+            {
+                Console.WriteLine(kytu);
+            }*/
+            /*thongbao = "Lady and Gentlyment";
+            Console.WriteLine(thongbao.ToLower());
+            Console.WriteLine(thongbao.ToUpper());
+            
+            Console.WriteLine(thongbao.Replace("and", "And"));
+            Console.WriteLine(thongbao.Insert(3, "gh"));
+            Console.WriteLine(thongbao.Substring(4,4));
+            Console.WriteLine(thongbao.Remove(10,3));
+            string[] cacchuoicon =thongbao.Split(' ');
+            foreach(var s in cacchuoicon)
+            {
+                Console.WriteLine(s);
+            }
+            string newstring=".";
+            string[] chuoicon =
+            {
+                "HOC",
+                "Lap",
+                "Trinh"
+            };
+            newstring = string.Join(newstring, chuoicon);
+            Console.WriteLine(newstring);*/
+
+
+            /*StringBuilder thongbao = new StringBuilder();
+            thongbao.Append("Xin");
+            thongbao.Append(" chao cac ban");
+            thongbao.Replace("Xin chao", "Chao mung");
+
+            string kq = thongbao.ToString();
+            Console.WriteLine(kq);*/
+
+
+        
+
+
+
+        }
+           
+    }
+}
