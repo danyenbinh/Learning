7d362ee0a73d60ed2f161bba594e672e4035f0bc
LAPTOP-KUH98PUK\asus <mvhoang085@gmail.com>
2023-08-30 19:29:03 +0700

    buổi học hôm thứ 2 e quên đăng ạ

---
+++ Ngay5.2/Ngay5.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay5._2
+{
+    class Program
+    {
+        class Abc
+        {
+            public void xinChao() => Console.WriteLine("Xin chao C#");
+        }
+        static void Main(string[] args)
+        {
+            Abc a = new Abc();
+            a = null;
+            a?.xinChao();
+            int? age;
+            age = null;
+            age = 100;
+            if (age.HasValue)
+            {
+                int _age = age.Value;
+                Console.WriteLine(_age);
+            }
+        }
+    }
+}
---
+++ Ngay5/Ngay5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymus_example
+{
+    class Program
+    {   
+        class Svien
+        {
+            public string Ten { set; get; }
+            public int Namsinh { set; get; }
+            public string Noising { set; get; }
+        }
+        class student
+        {
+            public string Name { set; get; }
+            public void Hello() => Console.WriteLine(Name);
+        }
+        static void inra(dynamic obj)
+        {
+            obj.Name = "adasdad";
+            obj.Hello();
+        }
+        static void Main(string[] args)
+        {
+            /*var sanpham = new { Ten ="ip",gia=1000,namsx=2018};
+            Console.WriteLine(sanpham.Ten);
+            Console.WriteLine(sanpham.gia);
+            Console.WriteLine(sanpham.namsx);
+            List<Svien> cacsinhvien = new List<Svien>()
+            {
+                new Svien() {Ten ="A",Namsinh= 2000, Noising = "LC"} ,
+                new Svien() { Ten = "B", Namsinh = 2001, Noising = "LC" },
+                new Svien() { Ten = "C", Namsinh = 2002, Noising = "LC" },
+
+            };
+            var ketqua = from sv in cacsinhvien
+                         where sv.Namsinh <= 2001
+                         select new
+                         {
+                             Ten = sv.Ten,
+                             NS = sv.Noising
+                         };
+            foreach(var sinhvien in ketqua)
+            {
+                Console.WriteLine(sinhvien.Ten + " - " + sinhvien.NS);
+            }*/
+            student a= new student();
+            inra(a);
+            dynamic tenbien;
+            tenbien = new { };
+
+            tenbien.asd = "asdad";
+            Console.WriteLine(tenbien.asd);
+        }
+    }
+}
