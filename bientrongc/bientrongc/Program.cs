a923dbb89c802277f13f533e9d7a2fbd33f73d71
DESKTOP-251NHRN\ADMIN <nquockhanh671@gmail.com>
2023-09-04 10:03:30 +0700

    bai6+7+8

--- bientrongc/bientrongc/Program.cs
+++ bientrongc/bientrongc/Program.cs
@@ -10,6 +10,54 @@ namespace bientrongc
     {
         static void Main(string[] args)
         {
+            bai9();
+         
+
+
         }
+        static void bai1()
+        {
+            Console.WriteLine("toi ten khanh");
+            Console.ReadKey();
+
+        }
+        static void bai6()
+        {
+            int a = 5, b = 5, c = 10, d = 15;
+
+            Console.WriteLine(a++);
+            Console.WriteLine(--b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a < b);
+            Console.ReadKey();
+
+        }
+        static void bai7()
+        {
+            const int x = 10;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+        static void bai8()
+        {
+            int i = 300;
+            byte b = (byte)i;
+            Console.WriteLine("i = " + i);
+            Console.WriteLine("b = " + b);
+            Console.ReadKey();
+        }
+        static void bai9()
+        {
+            string  K = "Kteam";
+
+            if (K =="Kteam2")
+                Console.WriteLine("Free Education");
+            else
+                Console.WriteLine(" connecting to . . .");
+            Console.ReadKey();
+        }
+  
+        }
+
     }
-}
