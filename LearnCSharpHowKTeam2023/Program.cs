6e72d3aa94da80dc3f057d838375690618ecc9f2
MSI\PC <nguyenanhtubkyb@gmail.com>
2023-09-17 00:34:03 +0700

    Bai 14, Bai 15

--- LearnCSharpHowKTeam2023/Program.cs
+++ LearnCSharpHowKTeam2023/Program.cs
@@ -641,6 +641,185 @@ namespace LearnCSharpHowKTeam2023
             }*/
 
             #endregion
+
+            #region Bài 14 Goto
+
+            /* goto TuNguen;
+             Console.WriteLine("Hoclaptrinh.com");
+             TuNguyen:
+             Console.WriteLine("Free education");*/
+
+            // Ví dụ 1
+            /*int a = 2;
+            switch (a)
+            {
+                case 1:             // label case 1
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:             // label case 2
+                    Console.WriteLine("Case 2");
+                    goto case 1;    // dịch chuyển tới label case 1
+                    break;          // Đoạn code này thừa vì sẽ không bao giờ thực thi
+                case 3:             // label case 3
+                    Console.WriteLine("Case 3");
+                    break;
+            }
+            Console.ReadKey();*/
+
+            // Ví dụ 2
+            /*int a = 2;
+            // nếu a == 2
+            if (a == 2)
+            {
+                // dịch chuyển tới vị trí label a_Is_2
+                goto a_Is_2;
+            }
+
+            Console.WriteLine("A == 1");
+            a_Is_2:
+            Console.WriteLine("A == 2");
+
+            Console.ReadKey();*/
+
+            // Ví dụ 3
+            /*int a = 2;
+
+            Ifinity_Loop:
+            // nếu a == 2
+            if (a == 2)
+            {
+                // dịch chuyển tới vị trí label a_Is_2
+                goto a_Is_2;
+            }
+
+            Console.WriteLine("A == 1");
+            a_Is_2:
+            Console.WriteLine("A == 2");
+            goto Ifinity_Loop; // dịch chuyển tới vị trí label Ifinity_Loop
+
+            Console.ReadKey();*/
+
+            // Ví dụ 4
+            /*string howKteamstr = "HowKteam.com";
+            int count = 0;
+        beginLoop:
+            Console.WriteLine(howKteamstr);
+            count++;
+            if (count < 10)
+                goto beginLoop;
+            Console.WriteLine("End loop");
+            Console.ReadKey();*/
+            #endregion
+
+            #region Vòng lặp For
+            // Cú pháp
+            /*for ([Khởi tạo]; [Điều kiện lặp];[Bước lặp lại])
+
+            {
+
+                // Khối lệnh được lặp lại. Có thể bỏ trống
+
+            }*/
+
+            // Ví dụ 1
+            /*// i sẽ được khởi tạo lần đầu tiên tại vòng lặp for
+            // khi vòng đời của vòng lặp for kết thúc bộ nhớ của biến i sẽ được giải phóng
+            // hay nói cách khác i là biến cục bộ của vòng lặp for
+
+            for (int i = 0; ;)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();*/
+
+            // Ví dụ 2
+            /*int i;
+
+            for (i = 0; i < 10;)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadKey();*/
+
+            // Ví dụ 3
+            /*int i;
+
+            for (i = 0; (i % 3 == 0) && (i < 10);)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadKey();*/
+
+            // Ví dụ 4
+            /*int i = 0;
+            int j = 0;
+            for (i = 0; i < 10; i++, j += 3)
+            {
+                Console.WriteLine("{0} | {1}",i, j);
+            }
+            Console.ReadKey();*/
+
+            // Ví dụ 5
+            /*Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < 10; i++, j += 3, Console.WriteLine("Tăng"))
+            {
+                Console.WriteLine("{0} | {1}", i, j);
+            }
+            Console.ReadKey();*/
+
+            // Ví dụ 6
+            /*int n = 100;
+            int j = 0;
+
+            for (int i = 0; i * j < n; i++, j += 3, Console.WriteLine("HowKTeam.com For Loop {0}", i))
+            {
+                Console.WriteLine("=======================================");
+                Console.WriteLine("i: {0} | j: {1} | i * j: {2}", i, j, i * j);
+            }
+            Console.ReadKey();*/
+
+            // Ví dụ 7
+            /*int N = 10;
+            int M = 20;
+
+            char drawChar = '*';
+            char insideChar = ' ';
+
+
+            // Vẽ từ trên xuống
+            for (int i = 0; i < N; i++)
+            {
+                // Vẽ từ trái sang
+                for (int j = 0; j < M; j++)
+                {
+                    *//*
+                     * Nếu đang ở tọa độ là cạnh trên hoặc dưới i % (N - 1) == 0
+                     * hoặc đang ở cạnh trái hoặc phải (j % (M - 1) == 0)
+                     * mà không nằm ở cạnh trên hoặc dưới (i % (N - 1) != 0)
+                     * ((i % (N - 1) != 0) && (j % (M - 1) == 0))
+                     * thì vẽ ra ký tự của hình chữ nhật
+                     * ngược lại vẽ ra ký tự không thuộc hình chữ nhật
+                    *//*
+
+                    if (i % (N - 1) == 0 || ((i % (N - 1) != 0) && (j % (M - 1) == 0)))
+                    {
+                        Console.Write(drawChar);    // lúc này là ký tự *
+                    }
+                    else
+                    {
+                        Console.Write(insideChar);  // lúc này là ký tự rỗng ' '
+                    }
+                }
+                //mỗi lần vẽ xong một hàng thì xuống dòng
+                Console.WriteLine();
+            }
+            Console.ReadKey();*/
+
+            #endregion
         }
 
     }
