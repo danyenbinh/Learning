930e8bea1543f67665d4c1057d28fd8b56e3dd97
LAPTOP-KUH98PUK\asus <mvhoang085@gmail.com>
2023-09-20 21:59:09 +0700

    b13,40,41,42 xuanthulab

---
+++ Ngay 13/Ngay 13/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay_13
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
+    public class MotaAttribute : Attribute
+    {
+        public string Thongtinchitiet { set; get; }
+        public MotaAttribute(string _Thongtinchitiet)
+        {
+            Thongtinchitiet = _Thongtinchitiet;
+        }
+    }
+    
+    public class user
+    {
+        [Required(ErrorMessage = "Name phai thiet lap")]    
+        [StringLength(50,MinimumLength =3, ErrorMessage ="Ten phai dai tu 3-100 ky tu")]
+        public string Name { set; get; }
+        [Range(18,80,ErrorMessage ="18-80")]
+        public int Age { set; get; }
+        [Phone(ErrorMessage ="loi")]
+        public string Phonenumber { set; get; }
+        [EmailAddress(ErrorMessage ="sai cau truc")]
+        public string Email { set; get; }
+        
+        public void PrintInfo() => Console.WriteLine(Name);
+    }
+    class Program
+    {
+        //Type->class,struct,....,int,bool,...
+        //Attribute
+        static void Main(string[] args)
+        {
+            /*int[] a= { 1, 2, 3 };
+            Type t1 = typeof(int);
+            var t2 = typeof(string);
+            var t3 = typeof(Array);
+
+            Type t = a.GetType();
+            Console.WriteLine(t.FullName);
+
+            Console.WriteLine("Cac thuoc tinh: ");
+            t.GetProperties().ToList().ForEach((o) =>
+            {
+                Console.WriteLine(o.Name);
+            });
+            Console.WriteLine("Cac truong du lieu: ");
+            t.GetFields().ToList().ForEach(( o) =>
+            {
+                Console.WriteLine(o.Name);
+            });
+
+            Console.WriteLine("Cac phuong thuc: ");
+            t.GetMethods().ToList().ForEach((o) =>
+            {
+                Console.WriteLine(o.Name);
+            });
+*/
+            user user = new user()
+            {
+                Name = "MVH",
+                Age = 20,
+                Phonenumber = "2312312312",
+                Email = "123asd",
+
+            };
+            /*var properties = user.GetType().GetProperties();
+            foreach (var p in properties)
+            {
+                foreach(var attr in p.GetCustomAttributes(false))
+                {
+                    MotaAttribute mota = attr as MotaAttribute;
+                    if (mota != null)
+                    {
+                        Console.WriteLine(mota.Thongtinchitiet);
+                        var value = p.GetValue(user);
+                        var name = p.Name;
+                        Console.WriteLine($"({name}) - {mota.Thongtinchitiet}:{value}");
+                    }
+                }
+
+            }*/
+            ValidationContext context = new ValidationContext(user);
+            var result = new List<ValidationResult>();
+            bool kq=Validator.TryValidateObject(user, context, result, true);
+            if (kq == false)
+            {
+                result.ToList().ForEach((er) =>
+                {
+                    Console.WriteLine(er.MemberNames.First());
+                    Console.WriteLine(er.ErrorMessage);
+                });
+            }
+
+
+        }
+    }
+}
---
+++ Ngay13.1/Ngay13.1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ngay13._2
+{
+    /*class A
+    {
+        public void ActionA() => Console.WriteLine("Action in class A");
+    }; 
+    class B
+    {
+        public void ActionB()
+        {
+            Console.WriteLine("Action in class B");
+            var a = new A();
+            a.ActionA();
+        }
+    };*/
+    interface IClassB
+    {
+        void ActionB();
+    };
+    interface IClassC
+    {
+        void ActionC();
+    }
+    class ClassC :IClassC
+    {
+        public void ActionC() => Console.WriteLine("Action in ClassC");   
+    }
+
+    class ClassB :IClassB
+    {
+        // Phụ thuộc của ClassB là ClassC
+        IClassC c_dependency;
+
+        public ClassB(IClassC classc) => c_dependency = classc;
+        public void ActionB()
+        {
+            Console.WriteLine("Action in ClassB");
+            c_dependency.ActionC();
+        }
+    }
+
+    class ClassA
+    {
+        // Phụ thuộc của ClassA là ClassB
+        IClassB b_dependency;
+
+        public ClassA(IClassB classb) => b_dependency = classb;
+        public void ActionA()
+        {
+            Console.WriteLine("Action in ClassA");
+            b_dependency.ActionB();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var b = new B(); 
+            b.ActionB();*/
+            IClassC objectC = new ClassC();
+            IClassB objectB = new ClassB(objectC);
+            ClassA objectA = new ClassA(objectB);
+
+            objectA.ActionA();
+            
+        }
+    }
+}
